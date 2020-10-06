8420fa01c6e3ddfa051cc5a100557ba5710aeafb
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-10-06 12:24:45 -0400

    Grabbed new input groups element from bootstrap. Changed the span text in the element to the time in hour. Changed the span from .00 to 'save'. Will change to icon later.

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    var currentDate = (moment().format('dddd, MMMM Do'))
+var dateStats = $("#currentDay").text(currentDate);    
+})
+
+//DOM Variables
+var timeDisplay = $("#container")
+
+
+//JS Variables
+var tableEl = $("<table>");
+var tableRowEl = $("<tr>");
+var newDiv = $("<div>");
+
+//Function 1(Create a Table)
+function CreateTable(){
+// console.log("Hello world!");
+  for(i=0;i<8;i++){
+    var timeCount = 9
+    timeCount++;}
+//     tableRowEl.text(timeCount);
+//     tableEl.append(tableRowEl);
+//     timeDisplay.append(tableEl);
+//   }
+  
+}
+// newDiv.append(tableEl);
+// $("#container").append(newDiv);
+
+
+CreateTable();
