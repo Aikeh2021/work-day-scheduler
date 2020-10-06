a910d6cb995e2f93c1e1b8f4eeee32ca41383a8e
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-10-06 18:59:09 -0400

    Did some edits to the style elements to fix the word-cut off that was happening on the buttons. Began writing some code to start the local storage process.

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
-$(document).ready(function(){
-    var currentDate = (moment().format('dddd, MMMM Do'))
-var dateStats = $("#currentDay").text(currentDate);    
-})
-
-//DOM Variables
-var timeDisplay = $("#container")
-
-
-//JS Variables
-var tableEl = $("<table>");
-var tableRowEl = $("<tr>");
-var newDiv = $("<div>");
-
-//Function 1(Create a Table)
-function CreateTable(){
-// console.log("Hello world!");
-  for(i=0;i<8;i++){
-    var timeCount = 9
-    timeCount++;}
-//     tableRowEl.text(timeCount);
-//     tableEl.append(tableRowEl);
-//     timeDisplay.append(tableEl);
-//   }
-  
-}
-// newDiv.append(tableEl);
-// $("#container").append(newDiv);
+console.log(moment().format('MMMM Do YYYY'));
+    
+//     var currentDate = (moment().format('dddd, MMMM Do'))
+//     var dateStats = $("#currentDay").text(currentDate);
+//     var currentHour = (moment().format('hA'));
+//     var hourArray = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 
 
-CreateTable();
+// function timeTextAreaColor (){
+//     console.log(moment().format('hA'));
+//         if(moment().format('hA') == hourArray.indexOf(i)){
+//             console.log("something matches!");
+//             // $("#" + hourArray[i]+ "").css("background-color:red");
+//         }
+    
+//     // if(moment().format('h') == hourArray[i]){
+//     //     console.log("we're the same");
+//         // $("#" + hourArray[i]+ "").css("background-color:red");
+//     }
+
+
+// timeTextAreaColor(i);
+
+
+
+
+
+
+
+//I have no idea what this block of code is or where it came from
+// const moment = require("./lib/moment.min.js/moment");
+
+// $(document).ready(function(){
+//     var currentDate = (moment().format('dddd, MMMM Do'))
+// var dateStats = $("#currentDay").text(currentDate);    
+// })
+
+var input1;
+//Target HTML elements and store in variables
+var saveButton9 = $("#9saveButton");
+var input9 = $("#9input");
+
+//Creating the event listener to save the hour inputs
+saveButton9.on("click", function(){
+    //when clicked, we store the name in local storage
+        console.log(input9);
+    //update the DOM
+});
+
