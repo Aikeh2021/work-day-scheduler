2a7c1800d32191b808d69240d7371fd5f96c7f33
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-10-07 13:32:58 -0400

    Re-pushing code because hourColors Function has stopped working.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # work-day-scheduler
+https://www.youtube.com/watch?v=n80RRNS1k64
+
+https://momentjs.com/
+
+https://support.google.com/webdesigner/thread/71102880?hl=en
+
+
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log(moment().format('MMMM Do YYYY'));
+
 $(document).ready(function () {
     $("#currentDay").text(moment().format('dddd, MMMM Do'));
 
@@ -13,16 +14,16 @@ $(document).ready(function () {
     function hourColors() {
         var currentHour = moment().hours();
         $(".time-block").each(function () {
-            var timeBlockHour = parseInt($(this).attr("id"))
+            var timeBlockHour = parseInt($(this).attr("id"));
             if (timeBlockHour < currentHour) {
                 $(this).addClass("past");
             } else if (timeBlockHour === currentHour) {
-                $(this).removeClass("past")
+                $(this).removeClass("past");
                 $(this).addClass("present");
             } else {
-                $(this).removeClass("past")
-                $(this).removeClass("present")
-                $(this).addClass("future")
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
             }
 
         })
@@ -30,103 +31,16 @@ $(document).ready(function () {
 
     }
 
-hourColors();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //     var currentHour = (moment().format('hA'));
-    //     var hourArray = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
-
-
-    // function timeTextAreaColor() {
-    //     console.log(moment().format('hA'));
-    //     if (moment().format('hA') == hourArray.indexOf(i)) {
-    //         console.log("something matches!");
-    //         // $("#" + hourArray[i]+ "").css("background-color:red");
-    //     }
-
-    //     // if(moment().format('h') == hourArray[i]){
-    //     //     console.log("we're the same");
-    //     // $("#" + hourArray[i]+ "").css("background-color:red");
-    // }
-
-
-    // // timeTextAreaColor(i);
-
-
-
-
-
-
-
-    //I have no idea what this block of code is or where it came from
-    // const moment = require("./lib/moment.min.js/moment");
-
-    // $(document).ready(function(){
-    //     var currentDate = (moment().format('dddd, MMMM Do'))
-    // var dateStats = $("#currentDay").text(currentDate);    
-    // })
-
-    // var input1;
-    // //Target HTML elements and store in variables
-    // var saveButton9 = $("#9saveButton");
-    // var input9 = $("#9input");
-
-    // //Creating the event listener to save the hour inputs
-    // saveButton9.on("click", function () {
-    //     //when clicked, we store the name in local storage
-    //     console.log(input9);
-    //     //update the DOM
-    // });
+    hourColors();
+
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#1 .description").val(localStorage.getItem("1"));
+$("#2 .description").val(localStorage.getItem("2"));
+$("#3 .description").val(localStorage.getItem("3"));
+$("#4 .description").val(localStorage.getItem("4"));
+$("#5 .description").val(localStorage.getItem("5"));
 
 });
