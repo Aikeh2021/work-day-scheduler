63f90ef38f19182084df9f21854d98879ac08302
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-10-07 11:14:20 -0400

    Restructured html code. Although the timeblocks worked, the code was very clunky when reviewed by another coder, my tutor. Finished the function to make the blocks change with the current hours. Added the event listener to the save buttons and set up local storage.

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 console.log(moment().format('MMMM Do YYYY'));
-    
-//     var currentDate = (moment().format('dddd, MMMM Do'))
-//     var dateStats = $("#currentDay").text(currentDate);
-//     var currentHour = (moment().format('hA'));
-//     var hourArray = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+$(document).ready(function () {
+    $("#currentDay").text(moment().format('dddd, MMMM Do'));
 
 
-// function timeTextAreaColor (){
-//     console.log(moment().format('hA'));
-//         if(moment().format('hA') == hourArray.indexOf(i)){
-//             console.log("something matches!");
-//             // $("#" + hourArray[i]+ "").css("background-color:red");
-//         }
-    
-//     // if(moment().format('h') == hourArray[i]){
-//     //     console.log("we're the same");
-//         // $("#" + hourArray[i]+ "").css("background-color:red");
-//     }
+    $(".saveBtn").on("click", function () {
+        var descriptionValue = $(this).siblings(".description").val()
+        var time = $(this).parent().attr("id")
+        localStorage.setItem(time, descriptionValue);
+    });
 
 
-// timeTextAreaColor(i);
+    function hourColors() {
+        var currentHour = moment().hours();
+        $(".time-block").each(function () {
+            var timeBlockHour = parseInt($(this).attr("id"))
+            if (timeBlockHour < currentHour) {
+                $(this).addClass("past");
+            } else if (timeBlockHour === currentHour) {
+                $(this).removeClass("past")
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("past")
+                $(this).removeClass("present")
+                $(this).addClass("future")
+            }
+
+        })
+
+
+    }
+
+hourColors();
 
 
 
@@ -27,23 +38,95 @@ console.log(moment().format('MMMM Do YYYY'));
 
 
 
-//I have no idea what this block of code is or where it came from
-// const moment = require("./lib/moment.min.js/moment");
 
-// $(document).ready(function(){
-//     var currentDate = (moment().format('dddd, MMMM Do'))
-// var dateStats = $("#currentDay").text(currentDate);    
-// })
 
-var input1;
-//Target HTML elements and store in variables
-var saveButton9 = $("#9saveButton");
-var input9 = $("#9input");
 
-//Creating the event listener to save the hour inputs
-saveButton9.on("click", function(){
-    //when clicked, we store the name in local storage
-        console.log(input9);
-    //update the DOM
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     var currentHour = (moment().format('hA'));
+    //     var hourArray = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
+
+    // function timeTextAreaColor() {
+    //     console.log(moment().format('hA'));
+    //     if (moment().format('hA') == hourArray.indexOf(i)) {
+    //         console.log("something matches!");
+    //         // $("#" + hourArray[i]+ "").css("background-color:red");
+    //     }
+
+    //     // if(moment().format('h') == hourArray[i]){
+    //     //     console.log("we're the same");
+    //     // $("#" + hourArray[i]+ "").css("background-color:red");
+    // }
+
+
+    // // timeTextAreaColor(i);
+
+
+
+
+
+
+
+    //I have no idea what this block of code is or where it came from
+    // const moment = require("./lib/moment.min.js/moment");
+
+    // $(document).ready(function(){
+    //     var currentDate = (moment().format('dddd, MMMM Do'))
+    // var dateStats = $("#currentDay").text(currentDate);    
+    // })
+
+    // var input1;
+    // //Target HTML elements and store in variables
+    // var saveButton9 = $("#9saveButton");
+    // var input9 = $("#9input");
+
+    // //Creating the event listener to save the hour inputs
+    // saveButton9.on("click", function () {
+    //     //when clicked, we store the name in local storage
+    //     console.log(input9);
+    //     //update the DOM
+    // });
+
 });
-
