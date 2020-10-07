b0188ba4e6e08aa5e8481dc9a4fbdb9bbb885001
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-10-07 15:32:35 -0400

    Finished adding comments to the html, css, and js files.

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
+//Troubleshooting to make sure the right moment.js features are being displayed
 console.log(moment().format('MMMM Do YYYY'));
 console.log(moment().hours());
 
+//Displaying the current date on the webpage (taken from moment.js)
 $(document).ready(function () {
     $("#currentDay").text(moment().format('dddd, MMMM Do'));
 
 
+    //Adding an event listener to the save buttons to make sure they're saving/setting the text from the timeblocks to local storage
     $(".saveBtn").on("click", function () {
         var descriptionValue = $(this).siblings(".description").val()
         var time = $(this).parent().attr("id")
         localStorage.setItem(time, descriptionValue);
     });
 
-
+//Changing the color of the timeblocks 
     function hourColors() {
         var currentHour = moment().hours();
         $(".time-block").each(function () {
@@ -34,6 +37,7 @@ $(document).ready(function () {
 
     hourColors();
 
+    //Calling the local storage to get the hourly event descriptions and put them on the page whenever the page is refreshed
 $("#9 .description").val(localStorage.getItem("9"));
 $("#10 .description").val(localStorage.getItem("10"));
 $("#11 .description").val(localStorage.getItem("11"));
